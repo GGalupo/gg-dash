dba4620397f15bc6544329bab8d3f3da11fb0519
Gustavo Galupo <ggalupo2@hotmail.com>
2022-01-25 02:25:10 -0300

    Pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,60 @@
+import { Box, Button, Flex, HStack } from "@chakra-ui/react";
+
+export const Pagination = () => {
+  return (
+    <Flex mt="8" justify="space-between" align="center" spacing="6">
+      <Box>
+        <strong>0</strong> - <strong>10</strong> of <strong>100</strong>
+      </Box>
+
+      <HStack>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          colorScheme="pink"
+          disabled
+          _disabled={{
+            bg: "pink.500",
+            cursor: "default",
+          }}
+        >
+          1
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bg="gray.700"
+          _hover={{
+            bg: "gray.500",
+          }}
+        >
+          2
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bg="gray.700"
+          _hover={{
+            bg: "gray.500",
+          }}
+        >
+          3
+        </Button>
+        <Button
+          size="sm"
+          fontSize="xs"
+          width="4"
+          bg="gray.700"
+          _hover={{
+            bg: "gray.500",
+          }}
+        >
+          4
+        </Button>
+      </HStack>
+    </Flex>
+  );
+};
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 
 const UserList = () => {
@@ -132,6 +133,8 @@ const UserList = () => {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
